dceb7bccf2417f26f21a6ded5d881cd81ca50fcd
Dominik Bartkowski <dominik.bartkowski@gmail.com>
2018-11-25 20:49:17 +0100

    how to page

--- rmxbot/apps/home/views.py
+++ rmxbot/apps/home/views.py
@@ -1,4 +1,6 @@
 
+
+from django.views.generic.base import TemplateView
 from django.http import HttpResponse
 from django.template import loader
 
@@ -9,3 +11,8 @@ def home(request):
     context = dict(success=True)
     template = loader.get_template("index.html")
     return HttpResponse(template.render(context, request))
+
+
+class HowTo(TemplateView):
+
+    template_name = 'about/howto.html'
