52653680ec3ee15f14afcc03d2728d700b8e00a5
Dominik Bartkowski <dominik.bartkowski@gmail.com>
2019-07-21 12:07:19 +0100

    -

--- rmxbot/contrib/db/connection.py
+++ rmxbot/contrib/db/connection.py
@@ -7,12 +7,6 @@ from ...config import (MONGODB_LOCATION, MONGODB_NAME, MONGODB_PASS,
                        MONGODB_PORT, MONGODB_USER)
 
 # connection for the mongodb user
-# uri = "mongodb://" % (MONGODB_LOCATION, MONGODB_PORT, MONGODB_USER, MONGODB_PASS)
-
-print('authenticating the user')
-print(MONGODB_USER)
-print(MONGODB_PASS)
-
 CLIENT = MongoClient(MONGODB_LOCATION, MONGODB_PORT)
 CLIENT.rmx.authenticate(MONGODB_USER, MONGODB_PASS, mechanism='SCRAM-SHA-1')
 
