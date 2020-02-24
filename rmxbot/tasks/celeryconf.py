aac6caf451355a6b2f1b5a1f862056d7fdcfce94
Dominik Bartkowski <dominik.bartkowski@gmail.com>
2020-02-24 22:15:09 +0000

    -

--- rmxbot/tasks/celeryconf.py
+++ rmxbot/tasks/celeryconf.py
@@ -4,9 +4,6 @@ from ..config import BROKER_HOST_NAME
 BROKER_URL = 'redis://{}:6379/0'.format(BROKER_HOST_NAME)
 CELERY_RESULT_BACKEND = 'redis://{}:6379/0'.format(BROKER_HOST_NAME)
 
-# BROKER_URL = f"amqp://rmxuser:rmxpass@{BROKER_HOST_NAME}:5672/rmxvhost"
-# CELERY_RESULT_BACKEND = 'rpc://'
-
 CELERY_IMPORTS = ('rmxbot.tasks.container', 'rmxbot.tasks.data')
 
 CELERY_TASK_RESULT_EXPIRES = 30
