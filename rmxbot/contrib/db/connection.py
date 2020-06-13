773a2fd8a056c471a85721e8853c67082e64bfd2
Dominik Bartkowski <dominik.bartkowski@gmail.com>
2020-06-13 17:51:45 +0100

    -

--- rmxbot/contrib/db/connection.py
+++ rmxbot/contrib/db/connection.py
@@ -3,18 +3,27 @@
 from pymongo import MongoClient
 
 from ...config import (MONGODB_LOCATION, MONGODB_NAME, MONGODB_PASS,
-                       MONGODB_PORT, MONGODB_USER)
+                       MONGODB_USER)
 
-CLIENT = MongoClient(MONGODB_LOCATION,
-                     username=MONGODB_USER,
-                     password=MONGODB_PASS,
-                     authSource=MONGODB_NAME)
+CLIENT = None
+
+
+def get_client():
+
+    global CLIENT
+    if not isinstance(CLIENT, MongoClient):
+        CLIENT = MongoClient(MONGODB_LOCATION,
+                             username=MONGODB_USER,
+                             password=MONGODB_PASS,
+                             authSource=MONGODB_NAME)
+    return CLIENT
 
 
 def get_connection(db=MONGODB_NAME, collection=None):
     """ returns a database connection and the collection """
     assert isinstance(collection, str), "No collection name provided."
-    database = CLIENT[db]
+    cli = get_client()
+    database = cli[db]
     collection = database[collection]
     return database, collection
 
