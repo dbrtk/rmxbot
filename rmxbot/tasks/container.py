1ff2c147edd6d9a278a624aac51c9794c384f356
Dominik Bartkowski <dominikbartkowski@gmail.com>
2021-02-07 23:11:11 +0000

    -

--- rmxbot/tasks/container.py
+++ rmxbot/tasks/container.py
@@ -253,7 +253,6 @@ def monitor_crawl(corpusid, iter: int = 0):
        receiving a list of endpoints from the scrapper.
     """
     iter += 1
-    print(f'inside monitor crawl: {corpusid}; {type(corpusid)}', flush=True)
     celery.send_task(
         SCRASYNC_TASKS['crawl_ready'],
         kwargs={'containerid': corpusid},
